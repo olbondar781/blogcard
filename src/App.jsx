9c855200999199962ea785fdb5af524161d7c9bd
oleksandrbondarit <105670677+oleksandrbondarit@users.noreply.github.com>
2025-11-30 22:57:14 +0100

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ export default function App() {
   return (
     <div style={{ padding: "40px" }}>
       <BlogCard
-        image="https://via.placeholder.com/400x200"
-        tag="Technology"
+        image="src\assets\images\Blog Image.png"
+        tag="HTML CSS"
         date="21 Nov 2024"
         title="The Future of Web Development"
         description="We explore the latest trends that will shape the future of web development in the next few years."
